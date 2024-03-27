19bf40be309eac45b38a83da218fad7f2f48ab5b
lmonsalve22 <lmonsalve22@gmail.com>
2024-03-27 19:38:09 -0300

    gecko

---
+++ gecko.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+
+
+
+if __name__ == '__main__':
+    url = "https://repositoriodeis.minsal.cl/SistemaAtencionesUrgencia/AtencionesUrgencia2024.zip"
+    download_path = '/ruta' 
+    options = Options()
+    options.add_argument('-headless')
+    options.set_preference('browser.download.dir', download_path)
+    options.set_preference('browser.download.folderList', 2)  # Configurar para que use la carpeta personalizada
+    options.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/octet-stream')  # Evitar diálogos de descarga
+
+    # Iniciar el navegador Firefox
+    driver = webdriver.Firefox(options=options)
+    #driver.implicitly_wait(3000000)
+    # Abrir la página web
+    try:
+        driver.get(url)
+    except:
+        print("No se pero llego aca")

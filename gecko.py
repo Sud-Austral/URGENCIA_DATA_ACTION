5e538d7f1556705350f961fd2ae256a2688f9313
lmonsalve22 <lmonsalve22@gmail.com>
2024-03-28 15:55:49 -0300

    os

--- gecko.py
+++ gecko.py
@@ -1,16 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-
+import os
 
 
 
 if __name__ == '__main__':
     url = "https://repositoriodeis.minsal.cl/SistemaAtencionesUrgencia/AtencionesUrgencia2024.zip"
     options = Options()
+    os.environ['PATH'] += os.pathsep + "/usr/local/bin"
     #options.add_argument('-headless')
     geckodriver_path = "/usr/local/bin/geckodriver"
     # Iniciar el navegador Firefox
-    driver = webdriver.Firefox(options=options, executable_path=geckodriver_path)
+    driver = webdriver.Firefox(options=options)
     #driver.implicitly_wait(3000000)
     # Abrir la página web
     try:
